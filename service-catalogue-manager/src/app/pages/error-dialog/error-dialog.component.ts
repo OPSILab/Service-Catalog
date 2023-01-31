fb653b09f60efd77f60072808fcb1b0f925b5ea5
Gabriele Percoco <gabriele.percoco@demetrix.it>
2023-01-31 10:41:34 +0100

    error dialogue update (reveal)

--- service-catalogue-manager/src/app/pages/error-dialog/error-dialog.component.ts
+++ service-catalogue-manager/src/app/pages/error-dialog/error-dialog.component.ts
@@ -1,87 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NbDialogRef } from '@nebular/theme';
 import { Location } from '@angular/common';
 import { LoginService } from '../../auth/login/login.service';
 @Component({
   selector: 'error-dialog',
-  template: `
-    <nb-card accent="danger" style=" max-width: 95vw; max-height: 95vh;">
-      <nb-card-header class="d-flex justify-content-between">
-        <h5 class="h5">Error</h5>
-        <button nbButton appearance="outline" shape="rectangle" size="tiny" status="info" class="close" (click)="closeModal(error)">
-          <i class="material-icons">close</i>
-        </button>
-      </nb-card-header>
-      <nb-card-body class="m-3">
-        <ng-container *ngIf="error.error && error.error !== 'EDITOR_VALIDATION_ERROR'; then genericError; else validationError"> </ng-container>
-
-        <ng-template #genericError>
-        <div class="row p-1">{{ error.error.message }}</div>
-        <br>
-        <nb-reveal-card>
-          <nb-card-front>
-            <nb-card>
-              <nb-card-body>
-                {{ 'general.editor.show_error_details' | translate }}
-              </nb-card-body>
-          </nb-card>
-        </nb-card-front>
-        <nb-card-back>
-          <nb-card>
-            <nb-card-body>
-             {{ error.message }}
-            </nb-card-body>
-          </nb-card>
-        </nb-card-back>
-
-      </nb-reveal-card>
-<!--
-          <nb-accordion>
-            <nb-accordion-item>
-              <nb-accordion-item-header>
-              {{ 'general.editor.show_error_details' | translate }}
-              </nb-accordion-item-header>
-              <nb-accordion-item-body>
-                <div class="row justify-content-center p-1">{{ error.message }}</div>
-                <strong>Status: {{ error.status }}</strong>
-              </nb-accordion-item-body>
-            </nb-accordion-item>
-          </nb-accordion>
--->
-</ng-template>
-
-        <ng-template #validationError>
-          <div class="row p-1 justify-content-center">{{ 'general.editor.validationErrors' | translate }}</div>
-          <div class="row p-3">
-            <table class="table table-striped">
-              <thead class="thead-light text-center">
-                <tr>
-                  <th scope="col">{{ 'general.error' | translate }}</th>
-                  <th scope="col">{{ 'general.editor.path' | translate }}</th>
-                </tr>
-              </thead>
-              <tbody class="text-center">
-                <tr *ngFor="let valError of error.validationErrors" class="">
-                  <td>{{ valError.message }}</td>
-                  <td>
-                    {{ valError.path }}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </ng-template>
-      </nb-card-body>
-    </nb-card>
-  `,
+  templateUrl: 'error-dialog.component.html',
+  //changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./error-dialog.component.scss']
 })
 export class ErrorDialogComponent {
   public error;
 
-  constructor(public ref: NbDialogRef<unknown>, private _location: Location, private loginService: LoginService) { }
+  constructor(public ref: NbDialogRef<unknown>, private _location: Location, private loginService: LoginService) {
+
+   }
 
   closeModal(error: { [key: string]: { cause?: string } }): void {
-    if (error.error?.cause === 'it.eng.opsi.cape.exception.AuditLogNotFoundException' || error.status === 0 || error.status === 401)
+    //if (error.error?.cause === 'it.eng.opsi.cape.exception.AuditLogNotFoundException' || error.status === 0 || error.status === 401)
+    if (error.status == 401)
       void this.loginService.logout().catch((error) => console.log(error));
     // else
     //   this.backClicked();
