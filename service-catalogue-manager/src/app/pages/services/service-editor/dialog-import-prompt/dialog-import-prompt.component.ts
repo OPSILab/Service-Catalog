cf9ac9e5fcef8506c93a371880d78ef69e5bd2ac
Gabriele Percoco <gabriele.percoco@demetrix.it>
2023-02-17 10:47:37 +0100

    trying to fix bugs

--- service-catalogue-manager/src/app/pages/services/service-editor/dialog-import-prompt/dialog-import-prompt.component.ts
+++ service-catalogue-manager/src/app/pages/services/service-editor/dialog-import-prompt/dialog-import-prompt.component.ts
@@ -5,6 +5,9 @@ import { NbDialogRef } from '@nebular/theme';
 import { ErrorDialogService } from '../../../error-dialog/error-dialog.service';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { ServiceModel } from '../../../../model/services/serviceModel';
+import { Byte } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'ngx-dialog-import-prompt',
@@ -13,12 +16,16 @@ import { BrowserModule } from '@angular/platform-browser';
 })
 export class DialogImportPromptComponent implements OnInit {
   selectedFile: File;
+  file: String;
+  fileOutput:String;
+  service: ServiceModel;
   json: Record<string, unknown>;
-  selectedItem = 'Json';
+  selectedItem //= 'Json';
   adapters: AdapterEntry[];
   adaptersActive: AdapterEntry[];
+  extension: String;
 
-  constructor(protected ref: NbDialogRef<DialogImportPromptComponent>, private errorService: ErrorDialogService, private availableAdaptersService: AvailableAdaptersService) { }
+  constructor(private http: HttpClient, protected ref: NbDialogRef<DialogImportPromptComponent>, private errorService: ErrorDialogService, private availableAdaptersService: AvailableAdaptersService) { }
 
   cancel(): void {
     this.ref.close();
@@ -26,10 +33,13 @@ export class DialogImportPromptComponent implements OnInit {
 
 
   async ngOnInit() {
+    console.log(String.fromCharCode(parseInt("00001010", 2)))
+    console.log(String.fromCharCode(parseInt("01100001", 2)))
+
     this.adaptersActive = [];
     this.adapters = await this.availableAdaptersService.getAdapters();
     this.adapters.forEach(adapterEntry => {
-      if (adapterEntry.status == "active" && adapterEntry.type=="MODEL")
+      if (adapterEntry.status == "active" && adapterEntry.type == "MODEL")
         this.adaptersActive.push(adapterEntry)
     });
   }
@@ -43,7 +53,9 @@ export class DialogImportPromptComponent implements OnInit {
       fileReader.readAsText(this.selectedFile, 'UTF-8');
       fileReader.onload = () => {
         try {
-          this.json = JSON.parse(fileReader.result as string) as Record<string, unknown>;
+          //this.json = JSON.parse(fileReader.result as string) as Record<string, unknown>;//G:
+          this.file = fileReader.result as String
+          this.extension = (<HTMLInputElement>event.target).files[0].name.split('.').pop().toLowerCase()//G:
         } catch (error) {
           this.errorService.openErrorDialog(error);
           this.ref.close();
@@ -58,8 +70,44 @@ export class DialogImportPromptComponent implements OnInit {
     }
   }
 
-  onUpload(): void {
+  async onUpload(): Promise<void> {
+    const fileReader = new FileReader();
     // upload code goes here
-    this.ref.close({ content: this.json, format: this.selectedItem });
+    /* console.log(
+       "\nthis.selectedFile\n",
+       this.selectedFile,
+       "\nthis.json\n",
+       this.json,
+       "\nthis.selectedItem\n",
+
+       "\nthis.adapters\n",
+       this.adapters,
+       "\nthis.adaptersActive\n",
+       this.adaptersActive
+       )*/
+    console.log("\n\n", this.file, "\n\n")
+    //for (let i = 0; i < this.selectedItem.length; i++) {
+
+    //console.log(this.selectedItem[i])
+
+    //}
+    //this.ref.close({ content: this.json, format: this.selectedItem });
+    //let a : Byte = 0b00001010;
+
+    //this.fileOutput=this.file.replace("\r\n", "newline")//+this.file[1];
+    if (this.extension=="csv") this.service = await this.http.post<ServiceModel>(this.adaptersActive[0].url, {
+      "sourceDataType": "csv",
+      "sourceData": this.file,//0b11111111
+      "mapPathIn": "examples\\serviceModelMap.json",
+      "dataModelIn": "ServiceModel",
+      "csvDelimiter": ";"
+    }).toPromise();//TODO continue this
+    /*if (this.extension == "csv") console.log( await this.http.post<JSON>(this.adaptersActive[0].url, {
+      "sourceDataIn": "examples\\serviceModel.csv",
+      "mapPathIn": "examples\\serviceModelMap.json",
+      "dataModelIn": "ServiceModel"
+    }).toPromise())*/
+    //this.ref.close({ content: this.file, format: this.extension });
+    this.ref.close({ content: this.service, format: this.extension });
   }
 }
