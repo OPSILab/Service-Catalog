a506d43e945b7c68437fa2224cf56e7ecee959e2
Gabriele Percoco <gabriele.percoco@demetrix.it>
2023-06-09 11:39:17 +0200

    add info status dialog

--- service-catalogue-manager/src/app/pages/management/availableCatalogues/status-render/status-render.component.ts
+++ service-catalogue-manager/src/app/pages/management/availableCatalogues/status-render/status-render.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { NbDialogService } from '@nebular/theme';
 
 @Component({
   selector: 'status-render',
   template: `
     <div *ngIf="active===true">
-      <button nbButton ghost shape="rectangle" size="small" status="success">
+      <button nbButton ghost shape="rectangle" size="small" status="success" (click)="showStatus()">
         <i class="material-icons">check_circle</i>
       </button>
     </div>
@@ -20,6 +21,23 @@ import { Component, Input, OnInit } from '@angular/core';
         <i class="material-icons">check_circle</i>
       </button>
     </div>
+
+    <ng-template #showStatusDialog let-data let-ref="dialogRef">
+      <nb-card>
+        <nb-card-header class="d-flex justify-content-between">
+          <h5>{{ 'general.catalogues.status' | translate }}</h5>
+        </nb-card-header>
+        <nb-card-body
+          class="p-5 text-center"
+          [innerHTML]="'general.catalogues.status' | translate : data.status"
+        ></nb-card-body>
+        <nb-card-footer class="d-flex justify-content-center">
+          <button nbButton class="ml-2" ghost shape="rectangle" status="primary" (click)="ref.close()">
+            {{ 'general.close' | translate }}
+          </button>
+        </nb-card-footer>
+      </nb-card>
+    </ng-template>
   `,
   styleUrls: ['../../../services/availableServices/availableServices.component.scss'],
 })
@@ -27,11 +45,27 @@ export class StatusRenderComponent implements OnInit {
   active: boolean | string = false;
 
   @Input() value;
+  @ViewChild('showStatusDialog', { static: false }) showStatusDialog: TemplateRef<unknown>;
+
+  constructor( private dialogService: NbDialogService,){}
 
   ngOnInit() {
     if (this.value.__zone_symbol__value != undefined) this.value = this.value.__zone_symbol__value
     //else this.active = this.value
     this.active = this.value == "unknown" ? this.value : (this.value == "active" || this.value == true)
+  }
+
+  showStatus(): void {
+    this.dialogService
+      .open(this.showStatusDialog, {
+        hasScroll: false,
+        context: {
+          status: this.value,
+        },
+      })
+      .onClose.subscribe((confirm) => {
+        //if (confirm) void this.onRegisterService();
+      });
   }
 }
 
