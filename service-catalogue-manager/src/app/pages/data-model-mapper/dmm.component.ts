0bb37ba31a497ab492d2ea656a2e88ce6870f7b3
Gabriele Percoco <114912675+Percocco@users.noreply.github.com>
2023-07-04 17:41:40 +0200

    fix bug : now updating mapper when schema changes

--- service-catalogue-manager/src/app/pages/data-model-mapper/dmm.component.ts
+++ service-catalogue-manager/src/app/pages/data-model-mapper/dmm.component.ts
@@ -1,6 +1,7 @@
+import { Mapper } from './../../model/adapter/mapper';
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/unbound-method */
-import { Component, OnInit, TemplateRef, ViewChild, ChangeDetectorRef, Inject } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, ChangeDetectorRef, Inject, OnChanges, SimpleChanges, Input, Output, EventEmitter } from '@angular/core';
 import { DMMService } from './dmm.service';
 import {
   NbDialogService,
@@ -25,15 +26,16 @@ import * as JSONEditor from '../../../../node_modules/jsoneditor/dist/jsoneditor
 import { DOCUMENT } from '@angular/common';
 import { DialogImportComponent } from './dialog-import/dialog-import.component';
 import { DialogDataMapComponent } from './dialog-dataMap/dialog-dataMap.component';
+import { AvailableServicesService } from '../services/availableServices/availableServices.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './dmm.component.html',
   styleUrls: ['./dmm.component.scss'],
 })
-export class DMMComponent implements OnInit {
+export class DMMComponent implements OnInit, OnChanges {
   @ViewChild('contentTemplate') contentTemplate: TemplateRef<any>;
-  
+
   editor: any;
   //editor2:any;
   container: any;
@@ -53,82 +55,131 @@ export class DMMComponent implements OnInit {
   mapOptions: string[];
   paths: string[];
   editor2: any;
+  maps: any;
+  mapper
+  schemas
+  selectedSchema
+
+  json3: any[];
+  editor3: any;
+  options3: any;
+  json2
 
   toggleView() {
     this.flipped = !this.flipped;
   }
 
-  constructor(@Inject(DOCUMENT) private document: Document, protected dialogService: NbDialogService, private windowService: NbWindowService) {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    protected dialogService: NbDialogService,
+    private windowService: NbWindowService,
+    private availableServicesService: AvailableServicesService,
+    private dmmService: DMMService,
+  ) { }
 
-  
-  
-  ngOnInit(): void {
+  schemaChanged($event) {
+    console.debug($event)
+    if (this.selectedSchema)
+      this.json3 = [
+        this.schema()
+      ];
+    if (!this.editor3) this.editor3 = new JSONEditor(this.container3, this.options3, this.json3);
+    else this.editor3.update(this.json3)
+    this.json2 = this.getAllNestedProperties(this.json3[0]);
+    //console.debug("Line 88",this.editor2)
+    this.editor2.update(this.json2)
+    //this.onUpdatePathForDataMap("");
+  }
+
+
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.debug(changes);
+  }
+
+
+
+  async ngOnInit(): Promise<void> {
     this.container = this.document.getElementById('jsoneditor');
     this.container2 = this.document.getElementById('jsoneditor2');
     this.container3 = this.document.getElementById('jsoneditor3');
     this.selectBox = <HTMLInputElement>this.document.getElementById('input-type');
     this.csvtable = this.document.getElementById('csv-table');
-    
 
-    
-  
-
-  
+    //await this.loadMapperList()
+    await this.loadSchemaList()
 
     //var selectedValue = 'csv';
 
     const options = {
       mode: 'view',
       modes: ['view', 'code'], // allowed modes
-      onModeChange: function (newMode, oldMode) {},
+      onModeChange: function (newMode, oldMode) { },
     };
 
-    const json = {};
+    const json = {
+      json1: 1
+    };
 
     this.editor = new JSONEditor(this.container, options, json);
 
-    const options3 = {
+    this.options3 = {
       mode: 'view',
       modes: ['view', 'preview'], // allowed modes
-      onModeChange: function (newMode, oldMode) {},
+      onModeChange: function (newMode, oldMode) { },
     };
 
-    const json3 = [
-      {
-        
-      }
-    ];
+    if (this.selectedSchema)
+      this.json3 = [
+        this.schema()
+      ];
 
-    const editor3 = new JSONEditor(this.container3, options3, json3);
+    if (this.json3) this.editor3 = new JSONEditor(this.container3, this.options3, this.json3);
 
+  }
+
+  schema() {
+    return this.schemas.filter(filteredSchema => filteredSchema.id == this.selectedSchema)[0].dataModel.properties
+  }
+
+  async loadMapperList() {
+    this.maps = await this.dmmService.getMaps();
+  }
+
+  async loadSchemaList() {
+    this.schemas = await this.dmmService.getSchemas();
   }
 
   getAllNestedProperties(obj) {
-    let properties = [];
+    let properties = {};
 
     for (let key in obj) {
-      properties.push(key);
-      if (typeof obj[key] === 'object') {
-        let nestedProps = this.getAllNestedProperties(obj[key]);
-        properties = properties.concat(nestedProps.map((prop) =>prop));
-      }
+
+      if (typeof obj[key] === 'object') //{
+        properties[key] = this.getAllNestedProperties(obj[key]);
+      else
+        properties[key] = "";
+      //properties = properties.concat(nestedProps.map((prop) => prop));
+      //}
     }
+    console.debug(properties)
     return properties;
 
-        
+
   }
 
-   //skipArrays:Ignore the array part 
-   //keepObjKeys:Whether to keep the parent object keys 
-  
-   getKeys(obj, keepObjKeys, skipArrays, keys=[], scope=[]) {
+  //skipArrays:Ignore the array part
+  //keepObjKeys:Whether to keep the parent object keys
+
+  getKeys(obj, keepObjKeys, skipArrays, keys = [], scope = []) {
 
     if (Array.isArray(obj)) {
       /*if (!skipArrays) scope.push('[' + obj.length + ']');
       obj.forEach((o) => this.getKeys(o, keepObjKeys, skipArrays, keys, scope), keys);*/
     } else if (obj && typeof obj === 'object' && obj.constructor === Object) {
       Object.keys(obj).forEach((k) => {
-        if ((!Array.isArray(obj[k]) && !(typeof obj[k]==='object')) || keepObjKeys) {
+        if ((!Array.isArray(obj[k]) && !(typeof obj[k] === 'object')) || keepObjKeys) {
           let path = scope.concat(k).join('.').replace(/\.\[/g, '[');
           if (!keys.includes(path)) keys.push(path);
         }
@@ -154,15 +205,16 @@ export class DMMComponent implements OnInit {
 
   onUpdatePathForDataMap(event) {
     console.log(event);
+    console.log(typeof event)
     console.log(this.editor.getText());
-    this.mapOptions= this.selectMapJsonOptions(this.editor.getText(), event);
+    this.mapOptions = this.selectMapJsonOptions(this.editor.getText(), event);
     console.log(this.mapOptions);
     this.setMapEditor();
   }
   setMapEditor() {
 
-    var dialogService= this.dialogService;
-    var mOptions=this.mapOptions
+    var dialogService = this.dialogService;
+    var mOptions = this.mapOptions
     console.log(mOptions);
 
     const options2 = {
@@ -170,25 +222,25 @@ export class DMMComponent implements OnInit {
       modes: ['tree', 'code', 'view', 'preview'], // allowed modes
       onModeChange: function (newMode, oldMode) {
 
-       
+
       },
-      
+
 
       onCreateMenu: function (items, node) {
         const path = node.path
 
         // log the current items and node for inspection
         console.log('items:', items, 'node:', node)
-      
+
         var selectPath = path;
         function pathToMap() {
-         
-         dialogService
-      .open(DialogDataMapComponent, {
-        context: { mapOptions: mOptions, selectPath:selectPath  },
-      })   
-        
-        
+
+          dialogService
+            .open(DialogDataMapComponent, {
+              context: { mapOptions: mOptions, selectPath: selectPath },
+            })
+
+
         }
 
         if (path) {
@@ -230,20 +282,8 @@ export class DMMComponent implements OnInit {
 
     };
 
-    const json2 = { 
-      "name": "",
-      "location": "",
-      "totalSlotNumber": "",
-      "address": {
-          "streetAddress": ""
-       },
-      "targetDataModel": "BikeHireDockingStation"
-    }
-
-  const editor2 = new JSONEditor(this.container2, options2, json2);
-
-  this.editor2=editor2;
-    
+    console.debug()
+    this.editor2 = new JSONEditor(this.container2, options2, this.json2);
   }
 
   saveAsFile(): void {
@@ -260,43 +300,43 @@ export class DMMComponent implements OnInit {
       .open(DialogImportComponent, {
         context: { type: typeSource },
       })
-      .onClose.subscribe((result: { content: string; source: string; format:string }) => {
+      .onClose.subscribe((result: { content: string; source: string; format: string }) => {
         if (result?.content) {
           this.sourceRef = result?.source;
-          this.sourceRefFormat=result?.format;
+          this.sourceRefFormat = result?.format;
           if (typeSource == 'csv') {
             this.csvSourceData = result.content;
             this.displayCSV(this.csvSourceData, this.csvtable, this.separatorItem);
-            this.mapOptions= this.csvSourceData.slice(0, this.csvSourceData.indexOf("\n")).split(this.separatorItem);
-            
+            this.mapOptions = this.csvSourceData.slice(0, this.csvSourceData.indexOf("\n")).split(this.separatorItem);
+
           } else {
             this.editor.setText(result.content);
-           
-            //this.mapOptions = this.getAllNestedProperties(JSON.parse(result.content));
-            this.paths=this.selectMapJsonOptions(result.content, '')
 
-            console.log( this.paths);
+            //this.mapOptions = this.getAllNestedProperties(JSON.parse(result.content));
+            this.paths = this.selectMapJsonOptions(result.content, '')
+
+            console.log(this.paths);
           }
         }
       });
   }
- 
- 
- 
-  selectMapJsonOptions(content: string, path:string): string[] {
 
-    return this.getKeys(_.get(JSON.parse(content), path+'[0]', JSON.parse(content) ), true, true)
 
-    
+
+  selectMapJsonOptions(content: string, path: string): string[] {
+
+    return this.getKeys(_.get(JSON.parse(content), path + '[0]', JSON.parse(content)), true, true)
+
+
   }
 
- 
+
 
   displayCSV(csvData: string, element: HTMLElement, separator: string) {
     // Split the CSV data into an array of rows
     var divElement = document.createElement('div');
-    divElement.style.overflowY="auto"; 
-    divElement.style.height="200px"; 
+    divElement.style.overflowY = "auto";
+    divElement.style.height = "200px";
 
     const rows = csvData.split('\n');
 
@@ -329,7 +369,7 @@ export class DMMComponent implements OnInit {
     element.appendChild(divElement);
   }
 
-    
+
 }
 
 
--- service-catalogue-manager/src/app/pages/data-model-mapper/dmm.service.ts
+++ service-catalogue-manager/src/app/pages/data-model-mapper/dmm.service.ts
@@ -6,13 +6,36 @@ import { AppConfig } from '../../model/appConfig';
 
 @Injectable()
 export class DMMService {
-  private dmmUrl: string;
+
   private config: AppConfig;
+
+  private dmmUrl: string;
 
   constructor(configService: NgxConfigureService, private http: HttpClient) {
     this.config = configService.config as AppConfig;
-   
+
   }
 
-  
+  getSchemas(): any {
+    return this.http.get<any[]>(this.config.data_model_mapper.default_mapper_base_url+"/dataModels").toPromise();
+
+  }
+  getMaps(): any {
+    return this.http.get<any[]>(this.config.data_model_mapper.default_mapper_base_url+"/maps").toPromise();
+  }
+
+  /*
+  getAdaptedService(type: string, source: string, adapter: AdapterEntry): Promise<ServiceModel[]> {
+    return this.http.post<ServiceModel[]>(adapter.url, {
+      "sourceDataType": type,
+      "sourceData": source,
+      "adapterID": adapter.adapterId,
+      "config": {
+        "delimiter": ";",
+        "NGSI_entity" : false
+      }
+    }).toPromise();
+  }*/
+
+
 }
